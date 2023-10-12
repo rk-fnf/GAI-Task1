cb62b63d71b5dab2184d2b38c28f3a01e799ed5d
rk-fnf <137400313+rk-fnf@users.noreply.github.com>
2023-10-12 14:26:33 +0530

    Initial Commit

---
+++ EmployeeFormSolution/EmployeeForm.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeForm.Application.Contracts.MessegeQueue;
+using EmployeeForm.Infrastructure.MessegeQueueService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeForm.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddTransient<IMessegeQueueSender, MessegeQueueSender>();
+            return services;
+        }
+    }
+}
---
+++ EmployeeFormSolution/EmployeeForm.Infrastructure/MessegeQueueService/MessegeQueueSender.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Messaging.ServiceBus;
+using EmployeeForm.Application.Contracts.MessegeQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeForm.Infrastructure.MessegeQueueService
+{
+    public class MessegeQueueSender:IMessegeQueueSender
+    {
+        public string ConnectionToQueue = "Endpoint=sb://servicebustask.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=NHoLFg8l+gxknbwo3EjS65wL3Wxmiechc+ASbKVhqgI=";
+        public async Task InsertTextToMessegeQueue(string content)
+        {
+            var client = new ServiceBusClient(ConnectionToQueue);
+            var sender = client.CreateSender("cosomosinfo");
+            var messege = new ServiceBusMessage(content);
+            await sender.SendMessageAsync(messege);
+        }
+    }
+}
