74cf873a66aee47f04800b10f303f5290d65155d
rk-fnf <137400313+rk-fnf@users.noreply.github.com>
2023-10-12 14:26:02 +0530

    Initial Commit

---
+++ EmployeeFormSolution/EmployeeForm.Domain/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeForm.Domain
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Qualification { get; set; }
+        public string Email { get; set; }
+        public string PhotoURl { get; set; } = string.Empty;
+    }
+}
---
+++ EmployeeFormSolution/EmployeeForm.Domain/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeForm.Domain
+{
+    public class Messages
+    {
+        public string onAddMessgege { get; set; } = "New employee was added to the database with Id";
+        public string onUpdateMessge { get; set; } = "Existing employee was updated in the database with Emp Id";
+        public string onDeleteMessge { get; set; } = "Employee was deleted from the database with Emp Id";
+    }
+}
